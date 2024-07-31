895e80ccf432839bea9d0c4ea4079c954040c8d4
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-07-31 19:53:36 -0300

    add prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
-export const app = fastify()
+export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Alice",
+    email: "alice@example.com",
+    dateOfBirth: new Date(2000, 1, 2),
+    shirtNumber: 10,
+  },
+});
