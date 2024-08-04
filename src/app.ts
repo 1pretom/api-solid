4857c252a73a085982e12a6c984a7a3680a438b2
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-08-04 00:13:23 -0300

    fix name

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const prisma = new PrismaClient();
 
 prisma.user.create({
   data: {
-    name: "Washington",
+    name: "Washington luis",
     email: "washington@example.com",
     dateOfBirth: new Date(2000, 1, 2),
     shirtNumber: 10,
