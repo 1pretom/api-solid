278ac5db7ab81cc051e6cd78345f7e086f634054
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-08-09 23:02:16 -0300

    feat: add hash to password

--- src/http/controler/register.ts
+++ src/http/controler/register.ts
@@ -1,9 +1,12 @@
 import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
-
-export const register = async (request:FastifyRequest, reply: FastifyReply) => {
+export const register = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
@@ -14,14 +17,25 @@ export const register = async (request:FastifyRequest, reply: FastifyReply) => {
   const { dateOfBirth, email, name, password, shirtNumber } =
     registerBodySchema.parse(request.body);
 
+  const password_hash = await hash(password, 6); //6 é o valor de rounds que o hash é gerado, e 6 é um valor bom para aplicações web
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send({ error: "Email already registered" });
+  }
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
       shirt_number: shirtNumber,
       date_of_birth: dateOfBirth,
     },
   });
-  return reply.status(201).send()
+  return reply.status(201).send();
 };
