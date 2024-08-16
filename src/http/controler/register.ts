17113ccc319502c757a5a851db7a368ff5c97149
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-08-15 23:40:29 -0300

    add threatment to error

--- src/http/controler/register.ts
+++ src/http/controler/register.ts
@@ -1,4 +1,5 @@
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 import { RegisterUseCase } from "@/use-cases/register";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
@@ -28,7 +29,10 @@ export const register = async (
       shirtNumber,
     });
   } catch (error) {
-    return reply.status(409).send();
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message });
+    }
+    return reply.status(500).send(); //TODO: fix
   }
   return reply.status(201).send();
 };
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 interface RegisterUseCaseRequest {
   password: string;
@@ -24,7 +25,7 @@ export class RegisterUseCase {
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
     if (userWithSameEmail) {
-      throw new Error("Email already registered");
+      throw new UserAlreadyExistsError();
     }
     // const prismaUsersRepository = new PrismaUsersRepository();
 
