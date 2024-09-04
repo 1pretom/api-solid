626d1902587c6aba22f299391d6266e15ae80823
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-09-04 12:05:49 -0300

    add payment use case

---
+++ src/use-cases/payment-use-case.ts
@@ -0,0 +1,31 @@
+import { PaymentsRepository } from "@/repositories/payments-repository";
+import { Payment } from "@prisma/client";
+
+interface PaymentCaseRequest {
+  userId: string;
+  groupId: string;
+  amount: number;
+}
+interface PaymentCaseResponse {
+  payment: Payment;
+}
+
+export class PaymentUseCase {
+  constructor(private paymentsRepository: PaymentsRepository) {}
+
+  async execute({
+    groupId,
+    userId,
+    amount,
+  }: PaymentCaseRequest): Promise<PaymentCaseResponse> {
+    const payment = await this.paymentsRepository.create({
+      group_id: groupId,
+      user_id: userId,
+      amount,
+    });
+
+    return {
+      payment,
+    };
+  }
+}
