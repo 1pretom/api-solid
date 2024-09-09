625609281a7fa85d8a4123120f1a46de5f9c44d1
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-09-09 08:11:11 -0300

    rename test

--- src/use-cases/__tests__/register.spec.ts
+++ src/use-cases/__tests__/register.spec.ts
@@ -44,7 +44,7 @@ describe("Register Use Case", () => {
   });
   it("should not be able to register with same email twice", async () => {
     await sut.execute({
-      name: "Washingtojn",
+      name: "Washington",
       dateOfBirth: "",
       email: "wash@example.com",
       password: "123456",
