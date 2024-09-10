32b132a6bcbc22c330ae5bb4208255866045b006
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-09-09 21:50:04 -0300

    add dayjs and verification to do not pay twice in same day

--- src/repositories/in-memory/in-memory-payments-repository.ts
+++ src/repositories/in-memory/in-memory-payments-repository.ts
@@ -1,14 +1,21 @@
 import { Payment, Prisma } from "@prisma/client";
 import { PaymentsRepository } from "../payments-repository";
 import { randomUUID } from "node:crypto";
+import dayjs from "dayjs";
 
 export class InMemoryPaymentsRepository implements PaymentsRepository {
   public items: Payment[] = [];
 
   async findByUserIdOnDate(userId: string, date: Date) {
-    const paymentOnSameDate = this.items.find(
-      (payment) => payment.user_id === userId
-    );
+    const startOfTheDay = dayjs(date).startOf("date");
+    const endOfTheDay = dayjs(date).endOf("date");
+
+    const paymentOnSameDate = this.items.find((payment) => {
+      const paymentDate = dayjs(payment.created_at);
+      const isOnSabeDate = paymentDate.isAfter(startOfTheDay) && paymentDate.isBefore(endOfTheDay)
+
+      return payment.user_id === userId && isOnSabeDate;
+    });
     if (!paymentOnSameDate) {
       return null;
     }
