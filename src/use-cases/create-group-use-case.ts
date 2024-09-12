a17827904d8fcceaf827e37a0d3598256671fbaf
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-09-12 13:18:05 -0300

    add create group

---
+++ src/use-cases/create-group-use-case.ts
@@ -0,0 +1,41 @@
+import { UsersRepository } from "@/repositories/users-repository";
+import { hash } from "bcryptjs";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+import { User } from "@prisma/client";
+
+interface CreateGymUseCaseRequest {
+  title: string;
+  description: string | null;
+}
+interface RegisterUseCaseResponse {
+  user: User;
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+  async execute({
+    password,
+    email,
+    name,
+    shirtNumber,
+    dateOfBirth,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const password_hash = await hash(password, 6); //6 é o valor de rounds que o hash é gerado, e 6 é um valor bom para aplicações web
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email);
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError();
+    }
+
+    const user = await this.usersRepository.create({
+      email,
+      name,
+      password_hash,
+      shirt_number: shirtNumber,
+      date_of_birth: dateOfBirth,
+    });
+    return {
+      user,
+    };
+  }
+}
