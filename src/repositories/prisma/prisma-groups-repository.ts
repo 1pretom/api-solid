84a75ff3922fedeb07488e9d50c696aa29f84c9a
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-09-16 00:46:49 -0300

    add groups repository repository

--- src/repositories/prisma/prisma-groups-repository.ts
+++ src/repositories/prisma/prisma-groups-repository.ts
@@ -1,17 +1,36 @@
 import { Prisma, Group } from "@prisma/client";
 import { FindManyNearbyParams, GroupsRepository } from "../groups-repository";
+import { prisma } from "@/lib/prisma";
+import { group } from "console";
 
 export class PrismaGroupsRepository implements GroupsRepository {
   async create(data: Prisma.GroupCreateInput) {
-    throw new Error("Method not implemented.");
+    const group = await prisma.group.create({
+      data,
+    });
+    return group;
   }
   async findById(id: string) {
-    throw new Error("Method not implemented.");
+    const group = await prisma.group.findUnique({
+      where: {
+        id,
+      },
+    });
+    return group;
   }
   async findManyNearby(params: FindManyNearbyParams) {
     throw new Error("Method not implemented.");
   }
   async searchMany(query: string, page: number) {
-    throw new Error("Method not implemented.");
+    const group = await prisma.group.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    });
+    return group;
   }
 }
