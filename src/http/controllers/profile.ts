f0f8898bd1c45e93e2c49e66928b37d8a470e2e6
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-09-17 20:07:42 -0300

    ending auth

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,27 +1,6 @@
-import { ResourceNotFoundError } from "@/use-cases/errors/resource-not-found-error";
-import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
 
-export const profile = async (
-  request: FastifyRequest,
-  reply: FastifyReply
-) => {
-  const getUserProfileBodySchema = z.object({
-    userId: z.string(),
-  });
-  const { userId } = getUserProfileBodySchema.parse(request.body);
-
-  try {
-    const getUserProfileUseCase = makeGetUserProfileUseCase();
-    await getUserProfileUseCase.execute({
-      userId
-    });
-  } catch (error) {
-    if (error instanceof ResourceNotFoundError) {
-      return reply.status(404).send({ message: error.message });
-    }
-    throw error;
-  }
+export const profile = async (request: FastifyRequest, reply: FastifyReply) => {
+  await request.jwtVerify();
   return reply.status(200).send();
 };
